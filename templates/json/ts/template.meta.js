268793460ca88da84dd53dd320fa1fdba82123de
shadowplay1 <shadowplay483@gmail.com>
2023-01-26 14:40:42 +0300

    feat(meta): template meta information file

---
+++ templates/json/ts/template.meta.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'json',
+    templateVersion: 'v1.0.0',
+    economyVersion: 'v1.7.4',
+    databaseType: 'json' 
+    language: 'typescript',
+    description: 'JSON bot template.' 
+}
+
