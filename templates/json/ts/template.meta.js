eef9e6e4e7cd7da915bd9cb8dee8dc3de6cb6ed9
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-01-26 18:18:11 +0600

    fix: syntax error

--- templates/json/ts/template.meta.js
+++ templates/json/ts/template.meta.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'json',
     templateVersion: 'v1.0.0',
     economyVersion: 'v1.7.4',
-    databaseType: 'json' 
+    databaseType: 'json',
     language: 'typescript',
     description: 'JSON bot template.' 
 }
